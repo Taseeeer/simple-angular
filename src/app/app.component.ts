bde3147a5d5dd9c2a2d1f6c3bd247282b926174f
Taseeeer <syedtaseeershah@gmail.com>
2021-08-31 23:42:49 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+
+  // currentPage = 0;
+
+  // images = [
+  //   {title: "Coke", url: "https://images.unsplash.com/photo-1588238142232-7108fb7dcbb6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80"},
+  //   {title: "7up", url: "https://images.unsplash.com/photo-1581636625402-29b2a704ef13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80"},
+  //   {title: "Sprite", url: "https://images.unsplash.com/photo-1565619096210-1db06475b423?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=946&q=80"},
+  //   {title: "Fanta", url: "https://images.unsplash.com/photo-1566846128021-b940b0eec910?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"}
+  // ];
+
+  // inc() {
+  //   this.currentPage = this.currentPage +1;
+  //   if(this.currentPage >= this.images.length) {
+  //     this.currentPage = 0;
+  //   }
+  // }
+
+
+  //Routing 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ElementsHomeComponent } from './elements/elements-home/elements-home.component';
+import { ElementsModule } from './elements/elements.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    ElementsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
